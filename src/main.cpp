795259d0bdde4926de24a76b6d6298f5a21fdd3f
Alex Martin <maddin4t0r@gmail.com>
2015-07-15 16:49:22 +0200

    setup base structure

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+
+#include "amx\amx.h"
+#include "plugincommon.h"
+#include "amxdbg.h"
+
+
+typedef void(*logprintf_t)(const char* format, ...);
+
+extern void *pAMXFunctions;
+logprintf_t logprintf;
+
+
+PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports()
+{
+	return SUPPORTS_VERSION | SUPPORTS_AMX_NATIVES | SUPPORTS_PROCESS_TICK;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData)
+{
+	pAMXFunctions = ppData[PLUGIN_DATA_AMX_EXPORTS];
+	logprintf = (logprintf_t)ppData[PLUGIN_DATA_LOGPRINTF];
+
+
+	logprintf(" >> plugin.log: successfully loaded.");
+	return true;
+}
+
+PLUGIN_EXPORT void PLUGIN_CALL Unload()
+{
+	logprintf("plugin.log: Unloading plugin...");
+
+
+	logprintf("plugin.log: Plugin unloaded.");
+}
+
+PLUGIN_EXPORT void PLUGIN_CALL ProcessTick()
+{
+}
+
+
+extern "C" const AMX_NATIVE_INFO native_list[] =
+{
+	{NULL, NULL}
+};
+
+PLUGIN_EXPORT int PLUGIN_CALL AmxLoad(AMX *amx)
+{
+	
+	return amx_Register(amx, native_list, -1);
+}
+
+PLUGIN_EXPORT int PLUGIN_CALL AmxUnload(AMX *amx)
+{
+	return AMX_ERR_NONE;
+}
