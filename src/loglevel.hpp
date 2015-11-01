ec0cf67636ef58074e26161ae4650397998ae357
Alex Martin <maddin4t0r@gmail.com>
2015-11-01 10:15:23 +0100

    add log level stuff

---
+++ src/loglevel.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+enum class LogLevel : unsigned int
+{
+	NONE = 0,
+	DEBUG = 1,
+	INFO = 2,
+	WARNING = 4,
+	ERROR = 8,
+	FATAL = 16
+};
+
+using LogLevel_ut = std::underlying_type<LogLevel>::type;
+
+inline bool operator&(const LogLevel lhs, const LogLevel rhs)
+{
+	return (static_cast<LogLevel_ut>(lhs) & static_cast<LogLevel_ut>(rhs))
+		== static_cast<LogLevel_ut>(rhs);
+}
+
+inline LogLevel operator|=(const LogLevel lhs, const LogLevel rhs)
+{
+	return static_cast<LogLevel>(
+		static_cast<LogLevel_ut>(lhs) | static_cast<LogLevel_ut>(rhs));
+}
