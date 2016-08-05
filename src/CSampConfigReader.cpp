384906af0d6d633e6db982cc1a5c76c9ac87d176
Alex Martin <maddin4t0r@gmail.com>
2016-08-05 14:35:31 +0200

    improve gamemode name retrieval

--- src/CSampConfigReader.cpp
+++ src/CSampConfigReader.cpp
@@ -61,7 +61,14 @@ bool CSampConfigReader::GetGamemodeList(vector<string> &dest)
 		value;
 	unsigned int counter = 0;
 
-	while (GetVar(varname + std::to_string(counter), value))
+	if (GetVar(varname, value)) //only "gamemode name number"
+	{
+		dest.push_back(value.substr(0, value.find(' ')));
+		return true;
+	}
+
+	//possibly multiple lines of "gamemode0 name number" "gamemode1 name number" etc.
+	while (GetVar(varname + std::to_string(counter), value)) 
 	{
 		dest.push_back(value.substr(0, value.find(' ')));
 		value.clear();
