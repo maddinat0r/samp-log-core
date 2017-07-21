6d9fe167dcb6407fcfb431209596d720112a437d
Y_Less <git@y-less.com>
2017-07-21 19:44:58 +0300

    Put enum OR declaration in `__cplusplus`

--- include/samplog/LogLevel.h
+++ include/samplog/LogLevel.h
@@ -18,12 +18,12 @@ enum samplog_LogLevel
 	VERBOSE = 32,
 };
 
+#ifdef __cplusplus
 inline samplog_LogLevel operator|(samplog_LogLevel a, samplog_LogLevel b)
 {
 	return static_cast<samplog_LogLevel>(static_cast<int>(a) | static_cast<int>(b));
 }
 
-#ifdef __cplusplus
 namespace samplog
 {
 	typedef samplog_LogLevel LogLevel;
