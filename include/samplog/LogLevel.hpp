dc793f01fccc05adc58f19997598dbf8b17a21dd
Alex Martin <maddin4t0r@gmail.com>
2017-10-08 12:16:32 +0200

    cpp-ify

---
+++ include/samplog/LogLevel.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef INC_SAMPLOG_LOGLEVEL_H
+#define INC_SAMPLOG_LOGLEVEL_H
+
+#ifdef ERROR //because Microsoft
+#undef ERROR
+#endif
+
+
+namespace samplog
+{
+	enum LogLevel
+	{
+		NONE = 0,
+		DEBUG = 1,
+		INFO = 2,
+		WARNING = 4,
+		ERROR = 8,
+		FATAL = 16,
+		VERBOSE = 32,
+	};
+
+	inline LogLevel operator|(LogLevel a, LogLevel b)
+	{
+		return static_cast<LogLevel>(static_cast<int>(a) | static_cast<int>(b));
+	}
+}
+
+
+#endif /* INC_SAMPLOG_LOGLEVEL_H */
