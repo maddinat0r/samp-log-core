7877e140d69bbb360167a4fb1e67581c71a3da4f
Alex Martin <maddin4t0r@gmail.com>
2017-10-08 15:26:58 +0200

    fix define name

--- include/samplog/export.h
+++ include/samplog/export.h
@@ -2,13 +2,13 @@
 
 #if defined _WIN32 || defined __CYGWIN__
 # ifdef __GNUC__
-#  ifdef IN_DCC_PLUGIN
+#  ifdef IN_LOGCORE_PLUGIN
 #   define DLL_PUBLIC __attribute__ ((dllexport))
 #  else
 #   define DLL_PUBLIC __attribute__ ((dllimport))
 #  endif
 # else
-#  ifdef IN_DCC_PLUGIN
+#  ifdef IN_LOGCORE_PLUGIN
 #   define DLL_PUBLIC __declspec(dllexport)
 #  else
 #   define DLL_PUBLIC __declspec(dllimport)
