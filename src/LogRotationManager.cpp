6ff8386f4ba88ad45a1bd04693070b02166c815e
Alex Martin <maddin4t0r@gmail.com>
2018-10-16 16:51:08 +0200

    implement date based log rotation
    affects #8

--- src/LogRotationManager.cpp
+++ src/LogRotationManager.cpp
@@ -3,7 +3,9 @@
 #include <cstdio>
 #include <fstream>
 #include <vector>
+#include <functional>
 #include <fmt/format.h>
+#include <fmt/time.h>
 #include <tinydir.h>
 
 
@@ -101,6 +103,82 @@ void LogRotationManager::Check(std::string const &filepath, LogRotationConfig co
 	}
 	else if (config.Type == LogRotationType::DATE)
 	{
+		using Clock = std::chrono::system_clock;
+		auto day_check_func = [](std::tm &tm)
+		{
+			// every night at 00:00 o'clock
+			return tm.tm_hour == 0 && tm.tm_min == 0;
+		};
 
+		auto tt = Clock::to_time_t(Clock::now());
+		auto tm = fmt::localtime(tt);
+		switch (config.Value.Date)
+		{
+		case LogRotationTimeType::DAILY:
+			if (!day_check_func(tm))
+				return;
+			break;
+		case LogRotationTimeType::WEEKLY:
+			// every monday night
+			if (tm.tm_wday != 1 || !day_check_func(tm))
+				return;
+			break;
+		case LogRotationTimeType::MONTHLY:
+			// every first of the month at night
+			if (tm.tm_mday != 1 || !day_check_func(tm))
+				return;
+			break;
+		}
+
+		std::vector<std::string> logfile_dates;
+		if (config.BackupCount > 0)
+		{
+			tinydir_dir dir;
+			tinydir_open(&dir, file_dir.c_str());
+
+			while (dir.has_next)
+			{
+				tinydir_file file;
+				tinydir_readfile(&dir, &file);
+				tinydir_next(&dir);
+
+				if (!file.is_reg)
+					continue;
+
+				std::string const fn(file.name);
+				if (fn.find(file_name) != 0)
+					continue;
+
+				if (fn == file_name)
+					continue;
+
+				logfile_dates.push_back(file.extension);
+			}
+
+			tinydir_close(&dir);
+
+			std::sort(logfile_dates.begin(), logfile_dates.end());
+		}
+
+		int count = logfile_dates.size();
+		for (auto const &d : logfile_dates)
+		{
+			if (count-- < config.BackupCount)
+				break;
+			
+			// unnecessary file, delete it
+			std::remove(fmt::format("{:s}.{:s}", filepath, d).c_str());
+		}
+
+		if (config.BackupCount != 0)
+		{
+			auto new_filename = fmt::format("{:s}.{:%Y%m%d-%H%M}", filepath, tm);
+			std::rename(filepath.c_str(), new_filename.c_str());
+		}
+		else
+		{
+			// clear original file, because the number of backups to keep is zero
+			std::ofstream logfile(filepath, std::ofstream::trunc);
+		}
 	}
 }
